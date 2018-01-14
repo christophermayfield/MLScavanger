6f28682798f07186a09f0ca0e560dc6ca378e6e4
christophermayfield <christopher.j.mayfield@gmail.com>
2018-01-13 22:37:20 -0800

    Add initial files

---
+++ CoreMLScavenge/Objects.swift
@@ -0,0 +1,13 @@
+//
+//  Objects.swift
+//  CoreMLScavenge
+//
+//  Created by Christopher Mayfield on 11/21/17.
+//  Copyright © 2017 Christopher Mayfield. All rights reserved.
+//
+
+import Foundation
+
+struct Objects {
+    let objectArray = ["computer keyboard", "mouse", "iPod", "printer", "digital clock", "digital watch", "backpack", "ping-pong ball", "envelope", "water bottle", "combination lock", "lampshade", "switch", "lighter", "pillow", "spider web", "sandal", "vacuum", "wall clock", "bath towel", "wallet", "poster", "chocolate"]
+}
